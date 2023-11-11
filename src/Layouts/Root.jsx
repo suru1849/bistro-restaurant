229955661f9b053cb8848df8a4efb8ab0de0a124
suru1849 <suranjan1849@gmail.com>
2023-11-12 02:22:10 +0600

    Banner added by using of react awesome component

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Shared/Components/Footer/Footer";
+import NavBar from "../Pages/Home/NavBar/NavBar";
 
 const Root = () => {
   return (
     <div>
+      <NavBar></NavBar>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
