2d6fe73b4447593a34ed1b6e130c856d55912653
suru1849 <suranjan1849@gmail.com>
2023-11-12 01:50:07 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Router from "./Router/Router";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-screen-xl mx-auto">
+    <React.StrictMode>
+      <RouterProvider router={Router}></RouterProvider>
+    </React.StrictMode>
+  </div>
+);
